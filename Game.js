bc2f81468cc5ceef8e59a018a8f75798df944e24
OulaMurad <omurad4056@conestogac.on.ca>
2020-06-14 02:00:30 -0400

    added some cases to the game

--- Game.js
+++ Game.js
@@ -1,62 +1,95 @@
 const GameState = Object.freeze({
-    WELCOMING:   Symbol("welcoming"),
-    FLAT:  Symbol("flat"),
+    WELCOMING: Symbol("welcoming"),
+    FLAT: Symbol("flat"),
     WAIT: Symbol("wait"),
     MANSION: Symbol("mansion"),
     BUTLER: Symbol("butler"),
-    TOAST: Symbol("toast")
+    TOAST: Symbol("toast"),
+    ADVENTURE: Symbol("yes"),
+    KNOCK: Symbol("knock"),
+    NOBEL: Symbol("Nobel"),
+    OK:Symbol("ok")
+
 });
 
-module.exports = class Game{
-    constructor(){
+module.exports = class Game {
+    constructor() {
         this.stateCur = GameState.WELCOMING;
     }
-    
-    makeAMove(sInput)
-    {
+
+    makeAMove(sInput) {
         let sReply = "";
-        switch(this.stateCur){
+        switch (this.stateCur) {
             case GameState.WELCOMING:
-                sReply = "It is a dark and rainy night. Bang! You have a flat tire. Too bad you don't have a spare. Do you WAIT or GO to the spooky mansion for help?";
+                sReply = "it's a dark and rainy night. Bang! You have a flat tire. Too bad you don't have a spare. Do you WAIT or GO to the spooky mansion for help?";
                 this.stateCur = GameState.FLAT;
                 break;
             case GameState.FLAT:
-                if(sInput.toLowerCase().match("wait")){
+                if (sInput.toLowerCase().match("wait")) {
                     sReply = "The road is deserted. After 1 hour there is still no help. Do you keep Waiting or do you go to the house?";
-                }else{
-                    sReply ="On the door is a large knocker. Do you knock or run back to your car to wait?";
+                } else {
+                    sReply = "On the door is a large knocker. Do you knock or run back to your car to wait?";
                     this.stateCur = GameState.MANSION;
                 }
                 break;
             case GameState.MANSION:
-                if(sInput.toLowerCase().match("knock")){
+                if (sInput.toLowerCase().match("knock")) {
                     sReply = "The door opens and you are greeted by a hunch-back butler. He asks you to come in. Do you go in or run back to the car?"
                     this.stateCur = GameState.BUTLER;
-                }else{
+                } else {
                     sReply = "The road is deserted. After 1 hour there is still no help. Do you keep Waiting or do you go to the house?";
                     this.stateCur = GameState.FLAT;
 
                 }
                 break;
             case GameState.BUTLER:
-                if(sInput.toLowerCase().match("run")){
+                if (sInput.toLowerCase().match("run")) {
                     sReply = "The road is deserted. After 1 hour there is still no help. Do you keep Waiting or do you go to the house?";
                     this.stateCur = GameState.FLAT;
 
-                }else{
+                } else {
                     sReply = "You seem to have walked in to a party. The host offers you some toast. Do you take the toast or ask to call a tow truck?";
                     this.stateCur = GameState.TOAST;
-    
+
                 }
                 break;
             case GameState.TOAST:
-                if(sInput.toLowerCase().match("toast")){
-                    sReply = "you enter a new world of adventure ... game over";
-                    this.stateCur = GameState.WELCOMING;
-                }else{
+                if (sInput.toLowerCase().match("toast")) {
+                    sReply = "you enter a new world of adventure .would you like to play a knock knock game ! ";
+                    this.stateCur = GameState.ADVENTURE;
+                } else {
                     sReply = "the phone lines are down ... Would you like some toast perhaps?";
                 }
+                break;
+            case GameState.ADVENTURE:
+                if (sInput.toLowerCase().match("yes")) {
+                    sReply = "knock knock";
+                    this.stateCur = GameState.KNOCK;
+                } else {
+                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
+                }
+                break;
+            case GameState.KNOCK:
+                if (sInput.toLowerCase().match("who's there?")) {
+                    sReply = "Nobel";
+                    this.stateCur = GameState.NOBEL;
+                } else {
+                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
+                }
+                break;
+            case GameState.NOBEL:
+                if (sInput.toLowerCase().match("nobel who?")) {
+                    sReply = "No Bell.that's why I knocked . I'm done with this game .Let's try something else ,ok? ";
+                   
+                    this.stateCur = GameState.OK;
+                } else {
+                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
+                    this.stateCur = GameState.FLAT;
+
+                }
+                break;
+
         }
-        return([sReply]);
+        return ([sReply]);
     }
 }
