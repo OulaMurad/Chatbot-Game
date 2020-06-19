87243bdfa6178035df9c51cf61b7f1736bcfbfd7
OulaMurad <omurad4056@conestogac.on.ca>
2020-06-19 19:46:46 -0400

    change the story steps

--- Game.js
+++ Game.js
@@ -1,74 +1,36 @@
 const GameState = Object.freeze({
     WELCOMING: Symbol("welcoming"),
-    FLAT: Symbol("flat"),
-    WAIT: Symbol("wait"),
-    MANSION: Symbol("mansion"),
-    BUTLER: Symbol("butler"),
-    TOAST: Symbol("toast"),
     ADVENTURE: Symbol("yes"),
     KNOCK: Symbol("knock"),
     NOBEL: Symbol("Nobel"),
     OK: Symbol("ok"),
-    MATH: Symbol("math")
-
+    YES: Symbol("yes"),
+    MATH: Symbol("math"),
+    HARD: Symbol("hard")
 });
 
 module.exports = class Game {
     constructor() {
         this.stateCur = GameState.WELCOMING;
-        this.nCurrent=0;
+        this.nCurrent = 0;
     }
 
     makeAMove(sInput) {
         let sReply = "";
         switch (this.stateCur) {
+
             case GameState.WELCOMING:
-                sReply = "it's a dark and rainy night. Bang! You have a flat tire. Too bad you don't have a spare. Do you WAIT or GO to the spooky mansion for help?";
-                this.stateCur = GameState.FLAT;
-                break;
-            case GameState.FLAT:
-                if (sInput.toLowerCase().match("wait")) {
-                    sReply = "The road is deserted. After 1 hour there is still no help. Do you keep Waiting or do you go to the house?";
-                } else {
-                    sReply = "On the door is a large knocker. Do you knock or run back to your car to wait?";
-                    this.stateCur = GameState.MANSION;
-                }
-                break;
-            case GameState.MANSION:
-                if (sInput.toLowerCase().match("knock")) {
-                    sReply = "The door opens and you are greeted by a hunch-back butler. He asks you to come in. Do you go in or run back to the car?"
-                    this.stateCur = GameState.BUTLER;
-                } else {
-                    sReply = "The road is deserted. After 1 hour there is still no help. Do you keep Waiting or do you go to the house?";
-                    this.stateCur = GameState.FLAT;
 
-                }
+                sReply = "Hi, Would you like to play a game?";
+                this.stateCur = GameState.ADVENTURE;
                 break;
-            case GameState.BUTLER:
-                if (sInput.toLowerCase().match("run")) {
-                    sReply = "The road is deserted. After 1 hour there is still no help. Do you keep Waiting or do you go to the house?";
-                    this.stateCur = GameState.FLAT;
 
-                } else {
-                    sReply = "You seem to have walked in to a party. The host offers you some toast. Do you take the toast or ask to call a tow truck?";
-                    this.stateCur = GameState.TOAST;
-
-                }
-                break;
-            case GameState.TOAST:
-                if (sInput.toLowerCase().match("toast")) {
-                    sReply = "you enter a new world of adventure .would you like to play a knock knock game ! ";
-                    this.stateCur = GameState.ADVENTURE;
-                } else {
-                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
-                }
-                break;
             case GameState.ADVENTURE:
                 if (sInput.toLowerCase().match("yes")) {
                     sReply = "knock knock";
                     this.stateCur = GameState.KNOCK;
                 } else {
-                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
+                    sReply = "would you like something else?";
                 }
                 break;
             case GameState.KNOCK:
@@ -76,7 +38,7 @@ module.exports = class Game {
                     sReply = "Nobel";
                     this.stateCur = GameState.NOBEL;
                 } else {
-                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
+                    sReply = " would you like something else?";
                 }
                 break;
             case GameState.NOBEL:
@@ -85,38 +47,58 @@ module.exports = class Game {
 
                     this.stateCur = GameState.OK;
                 } else {
-                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
-                    this.stateCur = GameState.FLAT;
+                    sReply = "would you like something else?";
+                    this.stateCur = GameState.ADVENTURE;
 
                 }
                 break;
             case GameState.OK:
                 if (sInput.toLowerCase().match("ok")) {
-                    sReply = "I have a math question for you,2+2? ";
-                     this.stateCur = GameState.MATH;
+                    sReply = "let us try this game , I'll ask you simple math questions and count the correct answers for you .At the end I'll tell you how many times you won let us get started! are you ready? ";
+                    this.stateCur = GameState.YES;
                 } else {
-                    sReply = "the phone lines are down ... Would you like some toast perhaps?";
-                    this.stateCur = GameState.FLAT;
+                    sReply = "would you like something else?";
+                    this.stateCur = GameState.ADVENTURE;
+
+                }
+                break;
+            case GameState.YES:
+                if (sInput.toLowerCase().match("yes")) {
+                    sReply = "2+2?";
+
+                    this.stateCur = GameState.MATH;
+                } else {
+                    sReply = "would you like something else";
+                    this.stateCur = GameState.ADVENTURE;
 
                 }
                 break;
             case GameState.MATH:
-                if (sInput=="4") {
-                    sReply = "you have correct answer ,now your turn to ask!";
+                if (sInput == "4") {
+                    sReply = "you have correct answer ,now this is the hard one can you make this math problen right ,8+8=91 ?";
+
+                    this.stateCur = GameState.HARD;
+                } else {
+                    sReply = "wrong  ... the correct answer is 4. whoohoo ! I win";
+                    this.stateCur = GameState.ADVENTURE;
+
+                }
+                break;
+            case GameState.HARD:
+                if (sInput == "16=8+8") {
+                    sReply = "WOW !you are so smart !you have correct answer ,now this is the hard one can you make this math problen right ,8+8=91 ?";
 
                     this.stateCur = GameState.WELCOMING;
                 } else {
-                 sReply= "wrong  ... the correct answer is 4. whoohoo ! I win";
-                    this.stateCur = GameState.FLAT;
+                    sReply = "wrong  ... the correct answer is 16=8+8. whoohoo ! I win";
+                    this.stateCur = GameState.ADVENTURE;
 
                 }
                 break;
 
-
-
-
         }
+
+
         return ([sReply]);
     }
-    
 }
